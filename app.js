01935e7ba91263edc92c727563d148d2b178363a
jetzerszeri <jetzers.2@gmail.com>
2023-10-23 21:07:42 -0400

    feat: add routerApi to the app.js file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dataBase = require('./dataBase');
+const routerApi = require('./routes');
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,16 @@ app.use(express.json());
 //me conecto a la DB
 dataBase.once('error', () => console.log('Error al conectar a la DB'));
 dataBase.once('open', () => console.log('Conectado a la DB'));
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'SniffNear API REST by Jake'
+    });
+});
+
+routerApi(app);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
