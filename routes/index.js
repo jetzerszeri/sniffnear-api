4b2d3745329396d47a70897fdbb7d00f450120a1
jetzerszeri <jetzers.2@gmail.com>
2023-10-24 15:48:49 -0400

    feat: create pets routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const usersRoutes = require('./usersRoutes.js');
+const petsRoutes = require('./petsRoutes.js');
 
 const routerApi = function (app) {
     app.use('/api/users', usersRoutes);
+    app.use('/api/pets', petsRoutes);
 }
 
 module.exports = routerApi;
