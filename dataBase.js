4d840e982727ce5ec99b748d3f3bbdca0f62cf65
jetzerszeri <jetzers.2@gmail.com>
2023-10-23 21:33:46 -0400

    feat: add the SniffNear database to the connection

--- dataBase.js
+++ dataBase.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://jetzerszeri:3sTgfAkOkd1kdtyG@cluster0.ptej3xd.mongodb.net/?retryWrites=true&w=majority',{ 
+mongoose.connect('mongodb+srv://jetzerszeri:3sTgfAkOkd1kdtyG@cluster0.ptej3xd.mongodb.net/sniffnear?retryWrites=true&w=majority',{ 
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 })
