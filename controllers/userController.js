db9835a94509dd8bc365a12800cfbb14343b98ee
jetzerszeri <jetzers.2@gmail.com>
2023-10-23 21:23:37 -0400

    feat: create function and route to add user

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,3 +4,24 @@ const salt = 10;
 const jwt = require('jsonwebtoken');
 const clave = 'claveSecreta'; // Clave secreta para el token, debe ir en un archivo .env
 
+exports.addUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const user = new userModel({
+            name,
+            email,
+            password: passwordHash
+        });
+
+        await user.save();
+
+        res.status(201).json({ message: 'Usuario creado con éxito', user });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Hubo un error en el servidor'});
+    }
+}
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
 router.get('/', (req, res) => {
     res.status(200).json({
         message: 'Endpoint de usuarios funcionando'
     });
 })
+
+router.post('/', userController.addUser);
 
 module.exports = router;
