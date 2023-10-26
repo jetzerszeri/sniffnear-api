19123ffe0c4b6b31f91cae023d4e87f6354a3422
jetzerszeri <jetzers.2@gmail.com>
2023-10-26 18:59:54 -0400

    feat: add getAllPets, getPetById, updatePet routes
    add getAllPets, getPetById, updatePet functions on the petController and create the routes

--- controllers/petController.js
+++ controllers/petController.js
@@ -27,6 +27,68 @@ exports.addPet = async (req, res) => {
 
     }catch(error){
         console.log(error);
-        res.status(500).json({message: 'Hubo un error en el servidor'});
+        res.status(500).json({message: 'Hubo un error en el servidor al crear la mascota'});
     }
 };
+
+exports.getAllPets = async (req, res) => {
+    try{
+        const pets = await petModel.find();
+        res.status(200).json({pets});
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message: 'Hubo un error en el servidor al traer todas las mascotas'});
+    }
+};
+
+exports.getPetById = async (req, res) => {
+    try{
+        const pet = await petModel.findById(req.params.id);
+
+        if(!pet){
+            return res.status(404).json({message: 'No existe una mascota con ese ID'});
+        }
+        res.status(200).json({pet});
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message: 'Hubo un error en el servidor al traer la mascota'});
+    }
+};
+
+exports.updatePet = async (req, res) => {
+    try{
+        id = req.params.id;
+        const { name, breed, type, age, description, img, owner, sex } = req.body;
+
+        if (!name || !type) {
+            return res.status(400).json({ msg: 'Los campos name y type son obligatorios' });
+        }
+
+        const filter = { _id: id };
+        const update = {
+            name,
+            breed,
+            type,
+            age,
+            description,
+            img,
+            sex,
+            updatedAt: Date.now()
+        };
+
+        const result = await petModel.findByIdAndUpdate(filter, update);
+
+        if(!result){
+            return res.status(404).json({message: 'No existe una mascota con ese ID'});
+        }
+
+        res.status(200).json({message: 'Mascota actualizada con éxito', result});
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message: 'Hubo un error en el servidor al actualizar la mascota'});
+    }
+}
+
--- routes/petsRoutes.js
+++ routes/petsRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const petController = require('../controllers/petController');
 
 router.post('/', petController.addPet);
+router.get('/', petController.getAllPets);
+router.get('/:id', petController.getPetById);
+router.put('/:id', petController.updatePet);
 
 module.exports = router;
