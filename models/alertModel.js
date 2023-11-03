c1233548939d3ba43835bd3da078e791ed977718
jetzerszeri <jetzers.2@gmail.com>
2023-11-03 15:30:50 -0400

    feat: add more fields to ge alert model

--- models/alertModel.js
+++ models/alertModel.js
@@ -2,7 +2,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const alertSchema = new Schema({
-    title: {
+    alertType: {
+        type: String,
+    },
+    type: {
+        type: String,
+    },
+    size: {
+        type: String,
+    },
+    color1: {
+        type: String,
+    },
+    color2: {
+        type: String,
+    },
+    breed: {
+        type: String,
+    },
+    latitude: {
+        type: Number,
+    },
+    longitude: {
+        type: Number,
+    },
+    date:{
+        type: Date,
+    },
+    time:{
+        type: String,
+    },
+    img: {
+        type: String,
+    },
+    personName: {
+        type: String,
+    },
+    email: {
         type: String,
     },
     description: {
